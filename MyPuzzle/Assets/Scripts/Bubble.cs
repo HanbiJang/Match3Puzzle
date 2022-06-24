86c75ac71d94b50875a26ed13800141283af8b65
HanbiJang <hanbijang.dev@gmail.com>
2022-06-24 17:25:31 +0900

    버블에 상태 관련 추가

--- MyPuzzle/Assets/Scripts/Bubble.cs
+++ MyPuzzle/Assets/Scripts/Bubble.cs
@@ -2,8 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum BubbleType {
+    Normal,
+    Ice,
+    Bomb,
+    Lock,
+}
+
+public enum BubbleState {
+    UnMatched,
+    Matched,
+}
+
 public class Bubble : MonoBehaviour
 {
+
+    public BubbleType m_type;
+    public BubbleState m_state;
+    bool isSelected; //선택 되었는지 여부
+
+    void Awake() {
+        isSelected = false;
+    }
+
+    bool GetIsSelected() { return isSelected; }
+    void SetIsSelected(bool b) { isSelected = b; }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +39,6 @@ public class Bubble : MonoBehaviour
     {
         
     }
+
+
 }
